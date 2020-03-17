1d572c0dbd7cd85114a2eb83b2eef9e265e42e57
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-03-17 13:12:25 -0300

    Implementando paginação na page de products

--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -6,7 +6,9 @@ import "./styles.css";
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1 /*Adicionando page = 1, como defaul ao inciar o app */
   };
 
   //Função que irá ser executada assim que o componente for exibido em tela.
@@ -14,14 +16,37 @@ export default class Main extends Component {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get("/products");
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    /*Desestruturando. Pegando o docs e adicionando todo o resto para a a nossa "productInfo"*/
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  };
+
+  /*Metodo que levará para a proxima pagina.*/
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    /*Pages: Retorna o número total de paginas */
+    if (page == productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   };
 
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
 
     return (
       <div className="product-list">
@@ -33,6 +58,14 @@ export default class Main extends Component {
             <a href="">Acessar</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
+        </div>
       </div>
     );
   }
